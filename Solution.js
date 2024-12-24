a534da49377db2a12eef2cae14badb2b48229be7
EricEA <107333959+EricAzayev@users.noreply.github.com>
2024-12-24 01:42:58 +0000

    3/4 steps complete

--- Solution.js
+++ Solution.js
@@ -1,6 +1,6 @@
 class Solution {
     constructor(){}
-    generateNNArray(n) {
+    generateNNArray(n) { //generates the answer array
         let toReturn = Array.from({ length: n }, () => Array(n).fill(0)); //new nn array filled with 0s
         
         for (let i = 0; i < n; i++) {
@@ -16,7 +16,7 @@ class Solution {
         return toReturn;
     }
 
-    isUnique(toCheck, toReturn, n) {
+    isUnique(toCheck, toReturn, n) { //checks if number appended to 1xN array is unique //helper for generateOneArray(n)
         for (let i = 0; i < n; i++) {
             for (let j = 0; j < toCheck.length; j++) {
                 if (toReturn[i][j] === toCheck[j]) return false;
@@ -26,7 +26,7 @@ class Solution {
         return true;
     }
 
-    generateOneArray(n) {
+    generateOneArray(n) { //helper for generateNNArray
         let oneArray = Array(n).fill(0);
         
         for (let i = 0; i < n; i++) {
@@ -42,7 +42,7 @@ class Solution {
         return oneArray;
     }
 
-    isUniqueInArray(i, randomNum, oneArray) {
+    isUniqueInArray(i, randomNum, oneArray) { //returns whether the 1 x N array is unique in NXN Array. //helper for generateNNArray(n)
         if (i === 0) return true;
         
         for (let j = 0; j < i; j++) {
@@ -52,49 +52,58 @@ class Solution {
         return true;
     }
 
-    partitionArray(size){ 
-        //generate numbers between 1 and size
 
-        let toReturn = Array.from({ length: size }, () => Array(size).fill(0));
-        const onlyUniques = new Set([]);
-        while(this.hasZeros(toReturn)){
+    //!!!! end of generateNNArray line of commands !!!!! (Step 1)
+
+
+    //purpose of function: to create snakes
+    partitionArray(size){ //size synonymous with n: dimension of the array
+        
+        let toReturn = Array.from({ length: size }, () => Array(size).fill(0)); //fill array toReturn with 0s.
+        //const onlyUniques = new Set([]); //generate empty set
+
+        let pathLabel = 0;
+        while(this.hasZeros(toReturn)){ //goal is to remove all 0s 
             let i = Math.floor(Math.random() * size); //random row
             let j = Math.floor(Math.random() * size); //random col
+            //because this random coordinate may not be taken, we ensure this doesn't happen
+            while(toReturn[i][j] != 0)
+                {i = Math.floor(Math.random() * size); j = Math.floor(Math.random() * size);} //define i and j as 2  //ensure a unique coordinate is selected 
+            
+            //pathLabel is the problem section number
+            pathLabel++; 
+           
+            
+            //Temp equation to determine length of individual paths. 
+            let problemSize = Math.floor(Math.random() * 2) + 2; //temporarily set for range [2,4]
 
-            while(toReturn[i][j] != 0){i = Math.floor(Math.random() * size); j = Math.floor(Math.random() * size);} //ensure a unique coordinate is selected
-            let sec = Math.floor(Math.random() * size) + 1;
-            while(onlyUniques.has(sec))sec = Math.floor(Math.random() * size) + 1; //ensure sec is a UNIQUE random number
-            let problemSize = Math.floor(Math.random() * 2) + 2; //2 - 4 //Temp equation to determine pSs, TO BE CHANGED!!!
-
-            toReturn = randomBranching(i, j, problemSize, sec, toReturn);
+            toReturn = randomBranching(i, j, problemSize, pathLabel, toReturn);
 
         }
         return toReturn;
     }
-    randomBranching(i, j, amt, mark, arr){ //return type: array[][] only because js is pass by value //amt determines how many times recursion should occur 
-        let toReturn = arr; //to be recursed
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0 || arr[i][j] > 0|| amt == 0) {
+    randomBranching(i, j, amt, mark, arr){ //amt determines how many times recursion should occur //mark is the pathLabel //returns altered arr 
+        if (i < 0 || i >= arr.length || j < 0 || j >= arr[0].length || arr[i][j] > 0|| amt == 0) {
             return arr;
         }
         arr[i][j] = mark; //mark must > 0, it sets the partition. 
-        let moveThere = Math.floor(Math.random() * 4) + 1; //choosing random direction
-        //1 is up
-        //2 is down
-        //3 is left
-        //4 is right
+        
+        //1 is up //2 is down  //3 is left  //4 is right
         canGoUp = i > 0 && arr[i-1][j] == 0;
         canGoDown = i < arr.length - 1 && arr[i+1][j] == 0;
         canGoLeft = j > 0 && arr[i][j-1] == 0;
         canGoRight = j < arr.length - 1 && arr[i][j+1] == 0;
         
-        if(!canGoUp && !canGoDown && !canGoLeft && !canGoRight){
-            return arr;
-        }
+        
+        const validDirections = [];
+        if (canGoUp) validDirections.push(1);
+        if (canGoDown) validDirections.push(2);
+        if (canGoLeft) validDirections.push(3);
+        if (canGoRight) validDirections.push(4);
+        //check if a valid direction exists
+        if (validDirections.length === 0) return arr;
 
-        const canGo = new Map([[1,canGoUp],[2,canGoDown],[3,canGoLeft],[4,canGoRight]]);
-
-
-        while(!canGo.get(moveThere))moveThere = Math.floor(Math.random() * 4) + 1;
+        const moveThere = validDirections[Math.floor(Math.random() * validDirections.length)]; //selects code for valid direction
         switch(moveThere){
             case 1:
                 this.randomBranching(i-1, j, amt-1, mark, arr);
@@ -110,6 +119,8 @@ class Solution {
                 break;
         }
     }
+
+
     hasZeros(wholeArray) {
         for(let i = 0; i < wholeArray.length; i++){
             for (let j = 0; j < wholeArray[i].length; j++) {
@@ -118,25 +129,118 @@ class Solution {
         }
         return true;
     }
+
+    //!!!! end of createTunnelArray line of commands !!!!! (Step 2)
     
-    determineSum(targ, partitionArray, gameArray){ //if 7, adds the nums assigned location 7 
+    determineSum(pathLabel, partitionArray, gameArray){ //if 7, adds the nums assigned location 7 
         let sum = 0;
+        let x;
+        let y;
+        let first = true;
         for(let i = 0; i < gameArray.length; i++){
             for(let j = 0; j < gameArray[0].length; j++){
-                if(partitionArray[i][j] == targ)sum += gameArray[i][j];
+                if(partitionArray[i][j] == pathLabel){
+                    if(first){
+                        x = i;
+                        y = j;
+                    }
+                    
+                    sum += gameArray[i][j];
+                }
             }
         }
-        return sum;
+        return [sum.toString() + "+", x,y]; //kenken.js unpacks this list to know where to display this problem
     }
-    determineMult(targ, partitionArray, gameArray){ //if 7, adds the nums assigned location 7 
-        let mult = 0;
+
+    determineDifference(pathLabel, partitionArray, gameArray){ //pathlength must be 2!
+        let pathLength = 0;
+        let max = 0;
+        let min = 0;
+        let x;
+        let y;
+        let first = true;
         for(let i = 0; i < gameArray.length; i++){
             for(let j = 0; j < gameArray[0].length; j++){
-                if(partitionArray[i][j] == targ)mult *= gameArray[i][j];
+                if(partitionArray[i][j] == pathLabel){
+                    pathLength++;
+                    if(first){
+                        max = gameArray[i][j];
+                        min = gameArray[i][j];
+                        x = i;
+                        y = j;
+                        first = false;
+                    }
+                    else if(pathLength == 2){
+                        gameArray[i][j] > max ? max = gameArray[i][j] : min = gameArray[i][j]; 
+                    }
+                    else if(pathLength > 2){ //in the worst case, return sum. 
+                        return determineSum(pathLabel, partitionArray, gameArray)
+                    }   
+                }
             }
         }
-        return mult;
+        return [(max-min).toString + "-", x,y];
+}
+    determineMult(pathLabel, partitionArray, gameArray){ //if 7, adds the nums assigned location 7 
+        let sum = 0;
+        let x;
+        let y;
+        let first = true;
+        for(let i = 0; i < gameArray.length; i++){
+            for(let j = 0; j < gameArray[0].length; j++){
+                if(partitionArray[i][j] == pathLabel){
+                    if(first){
+                        x = i;
+                        y = j;
+                    }
+                    
+                    sum *= gameArray[i][j];
+                }
+            }
+        }
+        return [sum.toString() + "+", x,y]; //kenken.js unpacks this list to know where to display this problem
     }
+
+    determineDivis(pathLabel, partitionArray, gameArray){
+        let pathLength = 0;
+        let max = 0;
+        let min = 0;
+        let x;
+        let y;
+        let first = true;
+        for(let i = 0; i < gameArray.length; i++){
+            for(let j = 0; j < gameArray[0].length; j++){
+                if(partitionArray[i][j] == pathLabel){
+                    pathLength++;
+                    if(first){
+                        max = gameArray[i][j];
+                        min = gameArray[i][j];
+                        x = i;
+                        y = j;
+                        first = false;
+                    }
+                    else if(pathLength == 2){
+                        gameArray[i][j] > max ? max = gameArray[i][j] : min = gameArray[i][j]; 
+                    }
+                    else if(pathLength > 2){ //in the worst case, return sum. 
+                        return determineSum(pathLabel, partitionArray, gameArray)
+                    }   
+                }
+            }
+        }
+        return [(max / min).toString + "-", x,y];
+    }
+
+    
+
+
+
+
+
+    //!!!! end of createProblemArray line of commands !!!!! (Step 2)
+    
+    
+
 
 
 }
--- kenken.js
+++ kenken.js
@@ -15,7 +15,7 @@ function setGridColumns(size) {
     const gridTemplateColumns = `${'3fr '.repeat(size).trim()}`; // Generates "3fr 3fr" for size 2
     document.getElementById('KenContainer').style.gridTemplateColumns = gridTemplateColumns;
 }
-//document.getElementById('sizeShow').textContent = "YOOOOOOOOOO"; //test
+
 
 const size = parseInt(getQueryParam('size'));
 const difficulty = getQueryParam('difficulty');
@@ -23,19 +23,22 @@ document.getElementById('siteTitle').textContent = "Your " + difficulty + " " + 
 
 
 const solution = new Solution();
-var gameArray = solution.generateNNArray(size);
+
+//create an array of answers to crosscheck with player's answers. 
+//answer key used to create questions
+var gameArray = solution.generateNNArray(size); 
 console.log(gameArray);
 //gameArray = solution.decorateArray(gameArray, difficulty); To Be Implemented
 const toAppend = document.getElementById("mainContainer");
 
-let columns = '1fr '.repeat(size).trim();
+let columns = '1fr'.repeat(size).trim();
 let gridElements = document.querySelectorAll('.KenContainer');
 gridElements.forEach(function(element) {
     element.style.display = 'grid';
-    element.style.gridTemplateColumns = columns; // Ensures 4 columns for a 4x4 grid
+    element.style.gridTemplateColumns = '1fr';//columns; // Ensures 4 columns for a 4x4 grid
 });
 
-console.log(solution.partitionArray(size));
+// console.log(solution.partitionArray(size));
 
 for(let i = 0; i < size; i++){
     const a = document.createElement("div");
@@ -44,6 +47,10 @@ for(let i = 0; i < size; i++){
     
     for(let j = 0; j < size; j++){
         const nestDiv = document.createElement("div");
+        nestDiv.style.width = '99%';
+        nestDiv.style.height = '99%';
+        nestDiv.style.margin = '20px';
+        
         nestDiv.className = "nestContainer" + (j+1).toString();
         nestDiv.style.border = '3px solid black';
         nestDiv.textContent = gameArray[i][j].toString();
